0413a04c3f2833fc2e76aaab36bebe07eb0e9f4b
Eden Mesfun <emesfun@gmail.com>
2018-02-15 17:27:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,8 +11,8 @@ class Artist
 
   def add_song(song)
     @songs << song
-    #song.artist = self
-    song = self.songs #FIX
+    song.artist = self
+    #song = self.songs #FIX
   end
 
   def songs
