497914696cd29254886d287efd13b96764171403
Eden Mesfun <emesfun@gmail.com>
2018-02-15 18:03:00 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,10 +5,12 @@ attr_accessor :date, :doctor
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
+    patient.add_appointment(self)
   end
 
   def patient (date, doctor)
 
   end
 
+genre.add_song(self)
 end
