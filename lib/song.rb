3d73c17242e43edb034a3b2fa8c4c70543ce9ebf
Eden Mesfun <emesfun@gmail.com>
2018-02-15 17:37:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,11 @@ class Song
   def initialize(name, genre)
     @name = name
     @genre = genre
-    genre.add_songs(self)
     artist.add_songs(self)
   end
 
   def genre
+        genre.add_songs(self)
   end
 
   def artist
