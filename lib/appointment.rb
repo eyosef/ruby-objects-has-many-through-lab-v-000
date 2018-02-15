0656817426743f1840f0f0a2b197aaf9b19d8aec
Eden Mesfun <emesfun@gmail.com>
2018-02-15 18:01:53 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,12 +2,12 @@ class Appointment
 
 attr_accessor :date, :doctor
 
-def initialize(date, doctor)
-  @date = date
-  @doctor = doctor
-end
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+  end
 
-def patient (date, doctor)
-end
+  def patient (date, doctor)
+  end
 
 end
